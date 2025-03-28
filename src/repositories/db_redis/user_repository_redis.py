e59a83837b3c45fae5a02a85bb0326bf1221a688
Alex <qwe@qwe>
2025-03-28 16:09:16 +0600

    _vars

--- src/repositories/db_redis/user_repository_redis.py
+++ src/repositories/db_redis/user_repository_redis.py
@@ -1,41 +1,41 @@
 class UserRepositoryRedis:
     def __init__(self, database, er_handler):
-        self.__db = database
-        self.__error_handler = er_handler
+        self._db = database
+        self._error_handler = er_handler
 
     def create(self, full_name: str, email: str):
-        user_id = self.__db.db.incr("user_counter")
+        user_id = self._db.db.incr("user_counter")
         key = f"user:{user_id}"
         data = {"full_name": full_name, "email": email}
-        self.__db.db.hmset(key, data)
+        self._db.db.hmset(key, data)
         return {f"{key} created": data}
 
     def read(self, user_id: int):
         key = f"user:{user_id}"
-        data = self.__db.db.hgetall(key)
+        data = self._db.db.hgetall(key)
         if not data:
-            return self.__error_handler.not_found(key)
+            return self._error_handler.not_found(key)
         return {key: data}
 
     def update(self, user_id: int, full_name: str, email: str):
         key = f"user:{user_id}"
-        user = self.__db.db.hgetall(key)
+        user = self._db.db.hgetall(key)
         if not user:
-            return self.__error_handler.not_found(key)
+            return self._error_handler.not_found(key)
         data = {"full_name": full_name, "email": email}
-        self.__db.db.hmset(key, data)
+        self._db.db.hmset(key, data)
         return {f"{key} updated": data}
 
     def delete(self, user_id: int):
         key = f"user:{user_id}"
-        user = self.__db.db.hgetall(key)
+        user = self._db.db.hgetall(key)
         if not user:
-            return self.__error_handler.not_found(key)
-        self.__db.db.delete(key)
+            return self._error_handler.not_found(key)
+        self._db.db.delete(key)
         return {"message": f"{key} deleted"}
 
     def read_by_email(self, email: str, ignore_key=None):
         ignore_key = f"b'user:{ignore_key}'"
-        for key in self.__db.db.scan_iter(match=f"user:*"):
-            if str(key) != ignore_key and self.__db.db.hget(key, "email").decode() == email:
-                return self.__db.db.hgetall(key)
+        for key in self._db.db.scan_iter(match=f"user:*"):
+            if str(key) != ignore_key and self._db.db.hget(key, "email").decode() == email:
+                return self._db.db.hgetall(key)
--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,31 +1,31 @@
 class UserService:
     def __init__(self, repository, notification, er_handler):
-        self.__repository = repository
-        self.__notification = notification
-        self.__error_handler = er_handler
+        self._repository = repository
+        self._notification = notification
+        self._error_handler = er_handler
 
     def create(self, full_name: str, email: str):
         if self.check_email_duplicate(email):
-            return self.__error_handler.duplicate_found("email", email)
-        result = self.__repository.create(full_name, email)
+            return self._error_handler.duplicate_found("email", email)
+        result = self._repository.create(full_name, email)
         self.send_invite_notification(email, full_name)
         return result
 
     def read(self, user_id: int):
-        return self.__repository.read(user_id)
+        return self._repository.read(user_id)
 
     def update(self, user_id: int, full_name: str, email: str):
         if self.check_email_duplicate(email, current_key=user_id):
-            return self.__error_handler.duplicate_found("email", email)
-        return self.__repository.update(user_id, full_name, email)
+            return self._error_handler.duplicate_found("email", email)
+        return self._repository.update(user_id, full_name, email)
 
     def delete(self, user_id: int):
-        return self.__repository.delete(user_id)
+        return self._repository.delete(user_id)
 
     def check_email_duplicate(self, email, current_key=None):
-        return self.__repository.read_by_email(email, current_key)
+        return self._repository.read_by_email(email, current_key)
 
     def send_invite_notification(self, email, full_name):
         msg = f"Пользователь {full_name} добавлен в базу"
-        self.__notification.send(msg, email)
+        self._notification.send(msg, email)
 
