b7c64d0301bb0bca330787d50e7cf3cca46e6813
Alex <qwe@qwe>
2025-03-28 15:48:49 +0600

    Update start_cli.py, start_fastapi.py

--- start_cli.py
+++ start_cli.py
@@ -2,9 +2,10 @@ from src.init_services import init_services
 from src.gateways import get_cli
 
 
-services = init_services('config.json', 'cli')
-
-cli = get_cli(*services)
+def init_cli():
+    services = init_services('config.json', 'cli')
+    return get_cli(*services)
 
 if __name__ == '__main__':
+    cli = init_cli()
     cli()
--- start_fastapi.py
+++ start_fastapi.py
@@ -5,14 +5,20 @@ from src.init_services import init_services
 from src.gateways import get_user_router, get_position_router
 
 
-user_service, position_service = init_services('config.json', 'fastapi')
+def init_fastapi():
+    user_service, position_service = init_services('config.json', 'fastapi')
 
-app = FastAPI()
+    fastapi_app = FastAPI()
 
-user_router = get_user_router(user_service)
-position_router = get_position_router(position_service)
-app.include_router(user_router)
-app.include_router(position_router)
+    user_router = get_user_router(user_service)
+    position_router = get_position_router(position_service)
+    fastapi_app.include_router(user_router)
+    fastapi_app.include_router(position_router)
+
+    return fastapi_app
+
+
+app = init_fastapi()
 
 if __name__ == "__main__":
     uvicorn.run("start_fastapi:app", host="0.0.0.0", port=8000, reload=True)
